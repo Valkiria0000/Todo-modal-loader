dd60d41a91452d5ceb560ec00376a8127b849b21
Valkiria0000 <{ID}+{Valkiria0000}@Valkiria0000.noreply.github.com>
2022-10-09 13:34:24 +0300

    add project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import Loader from "./Todo/Loader";
+import Modal from "./Modal/Modal";
 
+const AddTodo = React.lazy(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(import("./Todo/addTodo"));
+      }, 3000);
+    })
+);
 function App() {
+  let [todos, setTodos] = React.useState([]);
+  let [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 2000);
+      });
+  }, []);
+  function toggleTodo(id) {
+    setTodos(
+      (todos = todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }))
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false,
+        },
+      ])
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+        <Modal />
+        <React.Suspense fallback={<p>loading...</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
+
+        {loading && <Loader />}
+
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : loading ? null : (
+          <p>No todos!</p>
+        )}
+      </div>
+    </Context.Provider>
   );
 }
 
---
+++ src/Todo/Loader.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default () => (
+  <div style={{ display: "flex", justifyContent: "center", margin: ".5rem" }}>
+    <div class="lds-default">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+);
